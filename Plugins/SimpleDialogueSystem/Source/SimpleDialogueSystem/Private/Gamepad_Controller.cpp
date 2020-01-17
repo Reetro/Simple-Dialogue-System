4836bd07fdf2c897a7d0e464fb7167101d05ce42
Reetro <thegamerboy7@gmail.com>
2020-01-16 19:40:45 -0700

    Setup controller detection

---
+++ Plugins/SimpleDialogueSystem/Source/SimpleDialogueSystem/Private/Gamepad_Controller.cpp
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Gamepad_Controller.h"
+#include "Super_NPC.h"
+
+AGamepad_Controller::AGamepad_Controller()
+{
+  bIsUsingGamepad = false;
+  bResetGamepadDetectionAfterNoInput = true;
+  GamepadTimeout = 5.f;
+}
+
+bool AGamepad_Controller::InputAxis(FKey Key, float Delta, float DeltaTime, int32 NumSamples, bool bGamepad)
+{
+  bool ret = Super::InputAxis(Key, Delta, DeltaTime, NumSamples, bGamepad);
+  _UpdateGamepad(bGamepad);
+  return ret;
+}
+
+bool AGamepad_Controller::InputKey(FKey Key, EInputEvent EventType, float AmountDepressed, bool bGamepad)
+{
+  bool ret = Super::InputKey(Key, EventType, AmountDepressed, bGamepad);
+  _UpdateGamepad(bGamepad);
+  return ret;
+}
+
+void AGamepad_Controller::PlayerTick(float DeltaTime)
+{
+  Super::PlayerTick(DeltaTime);
+  if (IsLocalController() && bResetGamepadDetectionAfterNoInput && bIsUsingGamepad)
+  {
+    float now = GetWorld()->TimeSeconds;
+    if (now > LastGamepadInputTime + GamepadTimeout)
+    {
+      bIsUsingGamepad = false;
+      ControllerDisconnected(); 
+    }
+  }
+}
---
+++ Plugins/SimpleDialogueSystem/Source/SimpleDialogueSystem/Public/Gamepad_Controller.h
@@ -0,0 +1,61 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "Gamepad_Controller.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SIMPLEDIALOGUESYSTEM_API AGamepad_Controller : public APlayerController
+{
+  GENERATED_BODY()
+
+public:
+
+   AGamepad_Controller();
+  
+  /** Can be called to check and see if the player is currently using a gamepad */
+  UPROPERTY(Transient, BlueprintReadOnly)
+  bool bIsUsingGamepad;
+
+  /** Specifies whether we set `bIsUsingGamepad` to `false` if we receive no input for a period of time. If set to 'true', GamepadTimeout will control how long
+      we need to go without receiving input before we set `bIsUsingGamepad` to `false`. */
+  UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+  bool bResetGamepadDetectionAfterNoInput;
+
+  /** How long we can go without receiving a gamepad input before we assume they've stopped using the gamepad */
+  UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (EditCondition = "bResetGamepadDetectionAfterNoInput"))
+  float GamepadTimeout;
+
+  // Overrides
+  virtual bool InputAxis(FKey Key, float Delta, float DeltaTime, int32 NumSamples, bool bGamepad) override;
+  virtual bool InputKey(FKey Key, EInputEvent EventType, float AmountDepressed, bool bGamepad) override;
+  virtual void PlayerTick(float DeltaTime) override;
+
+  UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category = "Controller Events")
+  void ControllerConnected();
+
+  UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category = "Controller Events")
+  void ControllerDisconnected();
+
+protected:
+
+  /** Common logic needed in both `InputAxis()` and `InputKey()` */
+  FORCEINLINE void _UpdateGamepad(bool bGamepad)
+  {
+    bIsUsingGamepad = bGamepad;
+    if (bGamepad)
+    {
+      LastGamepadInputTime = GetWorld()->TimeSeconds;
+      ControllerConnected();
+    }
+  }
+
+  // Used to keep track of when we last saw gamepad input
+  UPROPERTY(Transient, BlueprintReadOnly)
+  float LastGamepadInputTime;
+};
